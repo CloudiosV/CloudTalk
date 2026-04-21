97e991e250fccedd1b7307ae757dfaa376e69876
CloudiosV <cloudiosss@gmail.com>
2026-04-21 11:46:48 +0700

    refactor: organize files into folder

---
+++ app/chat/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ChatRoom from '../../components/ChatRoom';
+
+export default function Home() {
+  return (
+    <main>
+      <ChatRoom/>
+    </main>
+  );
+}
